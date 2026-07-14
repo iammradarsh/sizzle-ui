040b657fd784edefd66269a2a4360a8c740791ff
iammradarsh <mr1223adarsh@gmail.com>
2026-07-14 18:21:47 +0530

    featuredcard

--- components/cards/featured/FeaturedCard.tsx
+++ components/cards/featured/FeaturedCard.tsx
@@ -36,14 +36,14 @@ export default function FeaturedCard({
   time,
 }: FeaturedCardProps) {
   return (
-    <div className="w-[220px] shrink-0">
+    <div className="w-[280px] shrink-0">
       {/* Card */}
-      <div className="relative h-[410px] overflow-hidden rounded-[22px]">
+      <div className="relative h-[380px] overflow-hidden rounded-md">
         {/* Image */}
         <Image src={image} alt={title} fill className="object-cover" />
 
         {/* Gradient */}
-        <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-transparent to-transparent" />
+        <div className="absolute inset-0 " />
 
         {/* Logo */}
         {/* <Image
@@ -56,11 +56,11 @@ export default function FeaturedCard({
 
         {/* Bottom Tags */}
         <div className="absolute bottom-5 left-5 flex gap-2">
-          <div className="rounded-lg bg-black/70 px-2 py-1 text-xs text-white backdrop-blur-md">
+          <div className="rounded-sm bg-black/40 px-2 py-1 text-xs text-white backdrop-blur-md">
             {duration}
           </div>
 
-          <div className="rounded-lg bg-black/70 px-2 py-1 text-xs text-white backdrop-blur-md">
+          <div className="rounded-sm bg-black/40 px-2 py-1 text-xs text-white backdrop-blur-md">
             {category}
           </div>
         </div>
